6d396658494c468177fa1126f153ddc3ccf726c3
Gurnoor Singh <gurnos1@uci.edu>
2025-11-08 12:21:00 -0800

    First commit

--- backend/src/api.py
+++ backend/src/api.py
@@ -5,8 +5,9 @@ $ fastapi dev src/api.py
 """
 
 import random
-
+import requests
 from fastapi import FastAPI
+import json
 
 # The app which manages all of the API routes
 app = FastAPI()
@@ -29,3 +30,30 @@ async def hello() -> dict[str, str]:
 async def get_random_item(maximum: int) -> dict[str, int]:
     """Get an item with a random ID."""
     return {"itemId": random.randint(0, maximum)}
+
+
+
+def get_discovery():
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOGQ3MzY5MjlhNTc4NDA1MjE5MTNjNmM3MmViNjU1OSIsIm5iZiI6MTc2MjYyNTEyOC40MjEsInN1YiI6IjY5MGY4NjY4YWVlMjM1YTFkZjMxYWIzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ANELYRpnzbHR_3kUDw68eMHO6_TVhbsC8fP9Y7r-ufI"
+    }
+
+    response = requests.get(url, headers=headers)
+    json_store = json.loads(response.text)
+    print(json_store['results'])
+    return json_store['results']
+
+
+
+
+@app.post("/submit")
+async def hello() -> dict[str, str]:
+    """Get hello message."""
+    get_discovery()
+
+
+
+    return {"message": "Hello from FastAPI"}
