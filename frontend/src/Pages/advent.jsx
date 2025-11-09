927e595140155982dddeb6bb6fd5e001070eed0b
Bharathi <bharathikshan@gmail.com>
2025-11-08 19:31:23 -0800

    Frontend 2

--- frontend/src/Pages/advent.jsx
+++ frontend/src/Pages/advent.jsx
@@ -1,5 +1,6 @@
 import "./Styles/advent.css";
 import ticketLogo from "../assets/ticket.svg";
+import { useEffect, useState } from "react";
 
 async function getMovieData(genre, runtime, release, providers, num_res) {
     const body = {'with_genres': genre, 
@@ -8,12 +9,28 @@ async function getMovieData(genre, runtime, release, providers, num_res) {
         'with_watch_providers': providers,
         'num_results': num_res
         }
+
+    console.log(body)
     const response = await fetch("/api/submit", {method: "POST", body: JSON.stringify(body)});
 
     const info = await response.json();
+    console.log(info)
+    return info
 }
 
 function Advent() {
+    const [movie_dict, setMovieDict] = useState(null)
+    useEffect(()=> {
+        
+        const x = async () => {
+            const data = await getMovieData(["Mystery"], 120, "2020-01-01", ["Netflix"], 5)
+            //console.log(data)
+            setMovieDict(data)
+        }
+
+        x()
+        
+    }, [])
     return (
         <div className="background">
             <div className="image1-box">
