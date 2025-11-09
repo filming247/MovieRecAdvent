fd6cba690bebe518b19ff47c10bb5c66a3d53e8f
Justin Li <jdli3@uci.edu>
2025-11-08 21:22:59 -0800

    Fix checkboxes for realsies

--- frontend/src/Pages/advent.jsx
+++ frontend/src/Pages/advent.jsx
@@ -1,6 +1,7 @@
 import "./Styles/advent.css";
 import ticketLogo from "../assets/ticket.svg";
 
+
 async function getMovieData(genre, runtime, release, providers, num_res) {
     const body = {'with_genres': genre, 
         'with_runtime.lte': runtime,
@@ -14,6 +15,76 @@ async function getMovieData(genre, runtime, release, providers, num_res) {
 }
 
 function Advent() {
+<<<<<<< Updated upstream
+=======
+    const [movie_dict, setMovieDict] = useState(null)
+
+    const [comedyInfo, setComedyInfo] = useState(false)
+    const [mysteryInfo, setMysteryInfo] = useState(false)
+    const [actionInfo, setActionInfo] = useState(false)
+    const [docuInfo, setDocuInfo] = useState(false)
+    const [romInfo, setRomInfo] = useState(false)
+    const [netInfo, setNetInfo] = useState(false)
+    const [HBInfo, setHBInfo] = useState(false)
+    const [parInfo, setParInfo] = useState(false)
+    const [pInfo, setPInfo] = useState(false)
+    const [fubInfo, setFubInfo] = useState(false)
+    const [display, setDisplay] = useState(0)
+    const [view, setView] = useState(false)
+
+    useEffect(()=> {
+        
+        const x = async () => {
+            let genre = []
+            if (comedyInfo) {
+                genre.push("Comedy");
+            }
+            if (mysteryInfo) {
+                genre.push("Mystery");
+            }
+            if (actionInfo) {
+                genre.push("Action");
+            }
+            if (docuInfo) {
+                genre.push("Documentary");
+            }
+            if (romInfo) {
+                genre.push("Romance");
+            }
+            let platform = [];
+            if (netInfo) {
+                platform.push("Netflix");
+            }
+            if (HBInfo) {
+                platform.push("HBO Max");
+            }
+            if (parInfo) {
+                platform.push("Paramount+")
+            }
+            if (pInfo) {
+                platform.push("Peacock");
+            }
+            if (fubInfo) {
+                platform.push("FuboTV");
+            }
+            const data = await getMovieData(genre, 120, "2020-01-01", platform, 5)
+            //console.log(data)
+            setMovieDict(data)
+        }
+
+        x()
+        
+    }, [comedyInfo, mysteryInfo, actionInfo, docuInfo, romInfo, netInfo, HBInfo, parInfo, pInfo, fubInfo])
+
+
+
+    function displaycard(x){
+
+        setDisplay(x)
+        setView(true)
+
+    }
+>>>>>>> Stashed changes
     return (
         <div className="background">
             <div className="image1-box">
