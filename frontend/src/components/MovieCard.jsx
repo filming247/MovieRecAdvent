0ebd082b23d52bc9c74f9054a95721fd3bdc9896
Bharathi <bharathikshan@gmail.com>
2025-11-08 21:32:10 -0800

    frontent working filters

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,7 +1,21 @@
+import "../Pages/Styles/MovieCard.css";
 
 function MovieCard({movie_info}) {
     return (
-        <div> 
+        <div className="display"> 
+            <div className="title">
+                {movie_info['title']}
+            </div>
+
+            <div className="release">
+                {movie_info['release_date']}
+            </div>
+
+            <div className="image">
+                <img src={"https://image.tmdb.org/t/p/original/" + movie_info['poster_path']} className="img-box" alt="Image logo"/>
+            </div>
+            
+            
             {movie_info['overview']}
         </div>
     )
